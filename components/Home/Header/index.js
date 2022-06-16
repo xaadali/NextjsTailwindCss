70c388934b90704a1210e29a7434044e2fd53ffb
Xaad Ali <saad.rafique@kryptomind.com>
2022-06-16 12:23:24 +0500

    Burger Menu

--- components/Home/Header/index.js
+++ components/Home/Header/index.js
@@ -5,22 +5,24 @@ const Header = () => {
   return (
     <>
       <div className={styles.wrapper}>
-        <div className={styles.hire}>
-          <div className={styles.heading}>
-            We code to bring your dreams to <span>LIFE.</span>
+        <div className={styles.container}>
+          <div className={styles.hire}>
+            <div className={styles.heading}>
+              We code to bring your dreams to <span>LIFE.</span>
+            </div>
+            <div className={styles.text}>
+              Once you share your dream, we envision it and then bring it to
+              life with the latest technologies. You will always feel ahead of
+              the game with your competitors.
+            </div>
+            <div className={styles.hirebtn}>
+              <button>Hire Me</button>
+            </div>
           </div>
-          <div className={styles.text}>
-            Once you share your dream, we envision it and then bring it to life
-            with the latest technologies. You will always feel ahead of the game
-            with your competitors.
-          </div>
-          <div className={styles.hirebtn}>
-            <button>Hire Me</button>
-          </div>
-        </div>
-        <div className={styles.headerlogo}>
-          <div className={styles.logo}>
-            <Image src="/giphy.gif" width={500} height={400} alt="logo" />
+          <div className={styles.headerlogo}>
+            <div className={styles.logo}>
+              <Image src="/giphy.gif" width={500} height={400} alt="logo" />
+            </div>
           </div>
         </div>
       </div>
--- components/_Common/MobileNav/index.js
+++ components/_Common/MobileNav/index.js
@@ -1,15 +1,56 @@
 import React from "react";
 import styles from "./mobilenav.module.scss";
-const MobileNav = ({ isMobile, handleMobileNav }) => {
-  return (
-    <>
-      {isMobile && (
-        <div className={styles.wrapper} onClick={handleMobileNav}>
-          Mobile Nav
-        </div>
-      )}
-    </>
-  );
+import { useSpring, animated } from "react-spring";
+import { CgClose } from "react-icons/cg";
+import { useRouter } from "next/router";
+import Link from "next/link";
+const MobileNav = ({ isMobileMenu, handleMobileNav }) => {
+  const router = useRouter();
+  const StylesProps = useSpring({
+    opacity: isMobileMenu ? 0.95 : 0,
+    marginTop: isMobileMenu ? 0 : -50,
+  });
+  if (isMobileMenu) {
+    return (
+      <>
+        <animated.div
+          style={StylesProps}
+          className={styles.wrapper}
+          id="content"
+        >
+          <div className={styles.closeMenu} onClick={handleMobileNav}>
+            <CgClose className={styles.closeIcon} />
+          </div>
+          <div className={styles.mobileItem}>
+            <div className={styles.items}>
+              <div
+                className={
+                  router.pathname === "/" ? styles.active : styles.inactive
+                }
+              >
+                <div className={styles.item}>
+                  <Link href="/" className={styles.text}>
+                    Home
+                  </Link>
+                </div>
+              </div>
+              <div
+                className={
+                  router.pathname === "/about" ? styles.active : styles.inactive
+                }
+              >
+                <div className={styles.item}>
+                  <Link href="/about" className={styles.text}>
+                    About
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </animated.div>
+      </>
+    );
+  } else return null;
 };
 
 export default MobileNav;
